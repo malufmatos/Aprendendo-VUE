ffc7b887731e7500775f8bb816570ce17d7716df
maria luiza <malufmatos22@gmail.com>
2025-01-07 23:02:50 -0300

    hierarquia de componentes

---
+++ 2_projeto_vue/src/components/Form.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>Meu formulário:</h2>
+    <form action="">
+        <div>
+            <InputText />
+        </div>
+        <div>
+            <InputText />
+        </div> 
+        <div>
+            <Submit />
+        </div>
+    </form>
+</template>
+
+<script>
+    import InputText from './InputText.vue'
+    import Submit from './Submit.vue'
+    export default{
+        name: 'Form',
+        components:{
+            InputText,
+            Submit
+        }
+    }
+</script>
---
+++ 2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,15 @@
+<template>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JavaScript</li>
+            <li>Java</li>
+            <li>Python</li>
+        </ul>
+</template>
+
+<script>
+    export default{
+        name: 'Info'
+    }
+</script>
