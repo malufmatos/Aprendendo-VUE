6df680965db69aa7de1fb7e7b6a66ac8874519fc
maria luiza <malufmatos22@gmail.com>
2025-01-07 23:26:04 -0300

    argumentos

--- 2_projeto_vue/src/components/Info.vue
+++ 2_projeto_vue/src/components/Info.vue
@@ -8,16 +8,23 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu protfólio <a v-bind:href= "meu_link" target="_blank"> basta clicar aqui</a></p>
+        <Picture />
 </template>
-
 <script>
+import Picture from './Picture.vue';
+
     export default{
         name: 'Info',
+        components:{
+            Picture 
+        },
         data(){
             return{
                 esta_trabalhando: true,
                 mostrar_email: true,
-                email: 'malufmatos22@gmail.com'
+                email: 'malufmatos22@gmail.com',
+                meu_link: 'https://google.com'
             }
         }
     }
